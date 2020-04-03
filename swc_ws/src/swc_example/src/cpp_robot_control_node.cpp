520e453c8e8c8e1c51c1fdc12778dfaf0b820275
noahzemlin <noah.zemlin@gmail.com>
2020-04-02 23:31:05 -0500

    Add cpp example and make it default

---
+++ swc_ws/src/swc_example/src/cpp_robot_control_node.cpp
@@ -0,0 +1,46 @@
+#include "ros/ros.h"
+#include "swc_msgs/Control.h"
+#include "swc_msgs/Waypoints.h"
+
+ros::Publisher g_control_pub;
+
+void controlTimerCallback(const ros::TimerEvent& timer_event) {
+    // Create a new message with speed 1 (m/s) and turn angle 15 (degrees CW)
+    swc_msgs::Control controlMsg;
+    controlMsg.speed = 1000;
+    controlMsg.turn_angle = 0;
+
+    // Publish the message to /sim/control so the simulator receives it
+    g_control_pub.publish(controlMsg);
+}
+
+int main(int argc, char **argv)
+{
+    // Initalize our node in ROS
+    ros::init(argc, argv, "cpp_robot_control_node");
+    ros::NodeHandle node;
+
+    // Create a Publisher that we can use to publish messages to the /sim/control topic
+    g_control_pub = node.advertise<swc_msgs::Control>(node.resolveName("/sim/control"), 1);
+
+    // Create and wait for Waypoints service
+    ros::ServiceClient waypoint_service = node.serviceClient<swc_msgs::Waypoints>("/sim/waypoints");
+    waypoint_service.waitForExistence();
+
+    // Request waypoints and display them
+    swc_msgs::Waypoints waypoints_msg;
+    waypoint_service.call(waypoints_msg);
+
+    std::cout << "Found the following waypoints:" << std::endl;
+    for (int i=0; i<waypoints_msg.response.waypoints.size(); i++) {
+        std::cout << waypoints_msg.response.waypoints[i] << std::endl;
+    }
+
+    // Create a timer that calls timer_callback() with a period of 0.1 (10 Hz)
+    ros::Timer control_timer = node.createTimer(ros::Duration(0.1), &controlTimerCallback, false);
+
+    // Let ROS take control of this thread until a ROS wants to kill
+    ros::spin();
+
+    return 0;
+}
--- swc_ws/src/swc_example/src/py_robot_control_node.py
+++ swc_ws/src/swc_example/src/py_robot_control_node.py
@@ -5,37 +5,40 @@ import math
 from swc_msgs.msg import Control
 from swc_msgs.srv import Waypoints
 
-control_pub = None
+_control_pub = None
+
 
 def timer_callback(event):
     # Create a new message with speed 1 (m/s) and turn angle 15 (degrees CW)
     control_msg = Control()
     control_msg.speed = 1
-    control_msg.turn_angle = 0
+    control_msg.turn_angle = 15
 
     # Publish the message to /sim/control so the simulator receives it
-    control_pub.publish(control_msg)
+    _control_pub.publish(control_msg)
+
 
 def main():
-    global control_pub
+    global _control_pub
 
     # Initalize our node in ROS
-    rospy.init_node('robot_control_node')
+    rospy.init_node('py_robot_control_node')
 
     # Create a Publisher that we can use to publish messages to the /sim/control topic
-    control_pub = rospy.Publisher("/sim/control", Control, queue_size=1)
+    _control_pub = rospy.Publisher("/sim/control", Control, queue_size=1)
 
     # Wait for Waypoints service and then request waypoints
     rospy.wait_for_service('/sim/waypoints')
     waypoints = rospy.ServiceProxy('/sim/waypoints', Waypoints)()
 
-    print(waypoints)
+    print(waypoints.waypoints)
 
     # Create a timer that calls timer_callback() with a period of 0.1 (10 Hz)
     rospy.Timer(rospy.Duration(0.1), timer_callback)
 
     # Let ROS take control of this thread until a ROS wants to kill
     rospy.spin()
+
 
 if __name__ == '__main__':
     try:
