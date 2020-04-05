36a779264e506d730c6499a818a0940326b1cd73
danielbrownmsm <56051347+danielbrownmsm@users.noreply.github.com>
2020-04-05 09:35:52 -0500

    created class Robot with funcs
    now just need to find way to read sensors and stuff, plus create an instance, and test

--- swc_ws/src/swc_example/src/py_robot_control_node.py
+++ swc_ws/src/swc_example/src/py_robot_control_node.py
@@ -80,6 +80,56 @@ class PIDController():
     
     # end ##################################################################### no
     
+class Robot():
+    def __init__(self, name):
+        self.name = name
+        self.curr_lat = 0.0
+        self.curr_lon = 0.0
+        self.goal_lat = 0.0
+        self.goal_lon = 0.0             #                   |
+        self.curr_angle = 0.0           #idk if this period V is right (like, the param)
+        self.speedPID = PIDController(0.5, 0.0001, 0.0001, 0.1, 0, 0.1) # we want no error/zero distance, but we have .1 tolerance
+        self.anglePID = PIDController(0.1, 0.000001, 0.000001, 0.1, 0, 0.1) # we want no diff between angle and angle to goal
+        
+    def updateCoords(self, lat, lon):
+        self.curr_lat = lat
+        self.curr_lon = lon
+    
+    def setGoal(self, lat, lon):
+        self.goal_lat = lat
+        self.goal_lon = lon
+    
+    def dist(self, x1, y1, x2, y2):
+        return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+    
+    def getDist(self):
+        return self.dist(self.curr_lon, self.curr_lat, self.goal_lon, self.goal_lat)
+    
+    def getXDist(self):
+        return self.goal_lon - self.curr_lon #idk if this is right, might need to flip
+    
+    def getDesiredAngle(self):
+        return math.asin(self.getXDist(), self.getDist())
+    
+    def getCurrAngle(self):
+        return self.curr_angle
+    
+    def updateCurrAngle(self, angle):
+        self.curr_angle = angle
+        
+    def getDesiredSpeed(self):
+        return self.speedPID.calculate(self.getDist())
+    
+    def getAngle(self):
+        return self.anglePID.calculate(self.getDesiredAngle() - self.curr_angle) #hoping this works
+        
+    def getAction(self):
+        control_msg = Control()
+        control_msg.speed = self.getDesiredSpeed()
+        control_msg.turn_angle = self.getAngle()
+        return control_msg
+    
+    # # # # # # # # # # # 3 3 3 3 # # # # # # # # #  ##  # # # # # # # # # #  #   # #  # # ###  # # # # # # # # ### # # ####### entropy
 def timer_callback(event):
     # Create a new message with speed 1 (m/s) and turn angle 15 (degrees CW)
     control_msg = Control()
